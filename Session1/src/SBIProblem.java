e2e7a039417346facdc035e9a76a13ef604eec7a
anamika-99 <43959159+anamika-99@users.noreply.github.com>
2020-08-19 12:38:47 +0530

    Session2

---
+++ Session1/src/SBIProblem.java
@@ -0,0 +1,31 @@
+
+public class SBIProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day1SalesHK = 223451;
+		int day2SalesHK = 323452;
+		int day3SalesHK = 423459;
+		
+		
+		int day1SalesMD = 1223451;
+		int day2SalesMD = 2323452;
+		int day3SalesMD = 3423459;
+		
+		day3SalesHK = 516789;
+		System.out.println("Home and Kitchen 3 Day Sale:");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("day1SalesHK: "+day1SalesHK);
+		System.out.println("day2SalesHK: "+day2SalesHK);
+		System.out.println("day3SalesHK: "+day3SalesHK);
+		
+		System.out.println();
+		
+		System.out.println("Mobile Device 3 Day Sale:");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("day1SalesMD: "+day1SalesMD);
+		System.out.println("day2SalesMD: "+day2SalesMD);
+		System.out.println("day3SalesMD: "+day3SalesMD);
+	}
+
+}
